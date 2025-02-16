1d8eae5ed76991ef5df29ec391feec67aa56c093
Zehra Sueda Gahramanli <zgahrama@c2r8s3.42prague.com>
2025-02-16 21:05:17 +0100

    added two two function

--- ex00/first.c
+++ ex00/first.c
@@ -6,7 +6,7 @@
 /*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 13:18:45 by zgahrama          #+#    #+#             */
-/*   Updated: 2025/02/16 16:52:30 by zgahrama         ###   ########.fr       */
+/*   Updated: 2025/02/16 21:04:24 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,14 +46,14 @@ int	*check_rules_four(int arr[], int output[])
 }
 
 int	check_same_num(int num_arr[], int num)
-		// before putting the number inside the array, array created from output
+// before putting the number inside the array, array created from output
 {
 	int i;
 
 	i = 0;
 	while (i < 4)
 	{
-		if (num_arr[i] == num )
+		if (num_arr[i] == num)
 		{
 			return (31); // for error
 		}
@@ -65,7 +65,88 @@ int	check_same_num(int num_arr[], int num)
 	}
 	return (1); // success
 }
+int	*num_input_one_up_down(int output[], int arr[])
+{
+	int	i;
 
+	i = 0;
+	while (i < 4)
+	{
+		if (arr[i] == 1)
+		{
+			output[i] = 4;
+		}
+		i++;
+	}
+	i = 4;
+	while (i < 8)
+	{
+		if (arr[i] == 1)
+		{
+			output[i + 8] = 4;
+		}
+		i++;
+	}
+	return (output);
+}
+int	*num_input_one_left_right(int output[], int arr[])
+{
+	int	i;
+	int	j;
+
+	i = 8;
+	j = 0;
+	while (i < 11)
+	{
+		if (arr[i] == 1)
+		{
+			output[j] = 4;
+		}
+		i++;
+		j += 4;
+	}
+	j = 3;
+	while (i < 16)
+	{
+		if (arr[i] == 1)
+		{
+			output[j] = 4;
+		}
+		i++;
+		j += 4;
+	}
+	return (output);
+}
+
+int	*check_two_two(int output[], int arr[])
+{
+	int	i;
+	int j;
+
+	j = 0;
+	i = 0;
+	while (i < 4)
+	{
+		if (arr[i] == 2 && arr[i + 4] == 2)
+		{
+			if(output[1] == 4)
+			{
+				output[j] = 3;
+				output[j+2] = 1;
+				output[j+3] = 2;
+			}
+			else if(output[2] == 4)
+			{
+				output[j] = 2;
+				output[j+1] = 1;
+				output[j+3] = 3;
+			}
+			
+		}
+		j+=4;
+		i++;
+	}
+}
 
 int	*put_nums(char *argv[])
 {
